1d48ea230be1452179dd45244d3919db86f5b3fc
Rahul <rahulmathur87@gmail.com>
2024-08-02 18:55:06 +0530

    convert to flask version

--- server.py
+++ server.py
@@ -1,8 +1,27 @@
 from info import InfoBot
+from flask import Flask, render_template
 
-name = input("Input name : ")
-info_bot = InfoBot(name)
+# name = input("Input name : ")
 
-print(f"Hey {name}\n"
-      f"I think you are {info_bot.get_gender()}\n"
-      f"And maybe {info_bot.get_age()} years old.")
+#
+# print(f"Hey {name}\n"
+#       f"I think you are {info_bot.get_gender()}\n"
+#       f"And maybe {info_bot.get_age()} years old.")
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+
+@app.route("/guess/<name>")
+def guess(name):
+    info_bot = InfoBot(name)
+    age = info_bot.get_age()
+    gender = info_bot.get_gender()
+    return render_template('guess.html', name=name, age=age, gender=gender)
+
+
+app.run(debug=True)
